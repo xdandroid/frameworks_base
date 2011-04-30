e56477198ec3b5463a3341b2be974cc9aa584743
Howard Chu <hyc@symas.com>
2011-04-29 20:27:36 -0700

    Fix worldphone mode switch crashes

--- telephony/java/com/android/internal/telephony/PhoneProxy.java
+++ telephony/java/com/android/internal/telephony/PhoneProxy.java
@@ -31,6 +31,7 @@ import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
 import android.util.Log;
 
+import com.android.internal.telephony.CallManager;
 import com.android.internal.telephony.cdma.CDMAPhone;
 import com.android.internal.telephony.gsm.GSMPhone;
 import com.android.internal.telephony.gsm.NetworkInfo;
@@ -86,6 +87,10 @@ public class PhoneProxy extends Handler implements Phone {
                 }
             }
 
+            CallManager cMgr = CallManager.getInstance();
+            if(cMgr != null)
+                cMgr.unregisterPhone(mActivePhone);
+
             if(mOutgoingPhone.equals("GSM")) {
                 logd("Make a new CDMAPhone and destroy the old GSMPhone.");
 
@@ -131,6 +136,9 @@ public class PhoneProxy extends Handler implements Phone {
                     mActivePhone.getIccPhoneBookInterfaceManager());
             mPhoneSubInfoProxy.setmPhoneSubInfo(this.mActivePhone.getPhoneSubInfo());
             mCommandsInterface = ((PhoneBase)mActivePhone).mCM;
+
+            if(cMgr != null)
+                cMgr.registerPhone(mActivePhone);
 
             //Send an Intent to the PhoneApp that we had a radio technology change
             Intent intent = new Intent(TelephonyIntents.ACTION_RADIO_TECHNOLOGY_CHANGED);
